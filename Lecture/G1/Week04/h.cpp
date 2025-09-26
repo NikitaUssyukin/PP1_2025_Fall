d238efdbce13537056f9b59e9068133eba4bdc17
NikitaUssyukin <nikus0108@gmail.com>
2025-09-26 09:42:48 +0500

    upd

--- Lecture/G1/Week04/h.cpp
+++ Lecture/G1/Week04/h.cpp
@@ -1,3 +1,28 @@
 /*
 Converting from decimal to any
 */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    int n, base;
+    cin >> n >> base;
+
+    string res = "";
+
+    string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    while(n > 0) {
+        int rem = n % base;
+        n /= base;
+        res = digits[rem] + res;
+    }
+
+    cout << res << endl;
+
+    return 0;
+}
